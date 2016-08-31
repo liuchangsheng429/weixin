228134b7420774b0934f15b4c8072ac7a7c0a835
liu <979937208@qq.com>
2016-09-01 06:59:40 +0800

    初始化项目

---
+++ WeiChat/WeiChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeiChat
+//
+//  Created by Mac on 16/9/1.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
