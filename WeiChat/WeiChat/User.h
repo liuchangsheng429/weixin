6e35ebaf78b0da2d3f0f1abdb242def79a962157
liu <979937208@qq.com>
2016-09-01 07:00:30 +0800

    新增类

---
+++ WeiChat/WeiChat/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  WeiChat
+//
+//  Created by Mac on 16/9/1.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
---
+++ WeiChat/WeiChat/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  WeiChat
+//
+//  Created by Mac on 16/9/1.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
